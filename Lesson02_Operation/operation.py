1d0666179df7501ef3bcbba35997014a4122488b
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-05 11:20:13 +0100

    add new operation.py file and modify var_types.py

---
+++ Lesson02_Operation/operation.py
@@ -0,0 +1,11 @@
+a = 3 + 5
+print(a)
+a = a + 2
+print(a)
+b = 4
+res = a - b
+print(res)
+res = b * 2.3
+print(res)
+res = a / 4
+print(res)
--- Lesson02_Operation/var_types.py
+++ Lesson02_Operation/var_types.py
@@ -1,2 +1,19 @@
 a = 42
 b = 23
+print(type(a))
+
+str = type(a)
+print(str)
+
+c = 3.5
+print(type(c))
+
+a = "Cohort 82M"
+str = type(a)
+print(a)
+print(str)
+
+bool = True
+boolStr = type(bool)
+print(bool)
+print(boolStr)
