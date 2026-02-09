f7bef9cde407826560294840008bd6cc412caba7
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-09 17:00:09 +0100

    HW 2, HW3

--- HW02/main.py
+++ HW02/main.py
@@ -6,5 +6,5 @@ waffles_kg = 1.5
 money_eur = 10
 total_usd = cookies_kg * cookies_price + waffles_kg * waffles_price
 total_eur = total_usd / eur_to_usd
-rest_eur = money_eur - total_eur
-print(rest_eur)
+result_eur = money_eur - total_eur
+print(round(result_eur, 2))
---
+++ HW03/main.py
@@ -0,0 +1,11 @@
+def cost(qty, price):
+    return qty * price
+
+
+candies = cost(7, 3)
+ice_cream = cost(3, 5)
+total = candies + ice_cream
+
+print(candies)
+print(ice_cream)
+print(total)
