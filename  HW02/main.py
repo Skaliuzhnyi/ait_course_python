1720b74b2c86939df54a48ad535890f199ad3f2c
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-09 13:27:49 +0100

    hw2

---
+++  HW02/main.py
@@ -0,0 +1,10 @@
+eur_to_usd = 1.09
+cookies_price = 3.25
+waffles_price = 4.40
+cookies_kg = 0.5
+waffles_kg = 1.5
+money_eur = 10
+total_usd = cookies_kg * cookies_price + waffles_kg * waffles_price
+total_eur = total_usd / eur_to_usd
+rest_eur = money_eur - total_eur
+print(rest_eur)
