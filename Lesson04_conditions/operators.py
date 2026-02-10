d47309526c4e2f7c932b7a2a7af4243c593645a7
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-10 10:08:08 +0100

    logical operators

--- Lesson04_conditions/operators.py
+++ Lesson04_conditions/operators.py
@@ -11,3 +11,17 @@ a += 3
 # a = a - 4
 a -= 4
 print("a =", a)
+
+# !! logical operators "and", "or", "not"
+
+age = 27
+check = age >= 18 and age < 45  # оба условия верны и в результате check - true
+print("check =", check)
+
+age = 16
+check = age >= 18 and age < 45 # одно из условий не верно и в результате check - false
+print("check =", check)
+
+age = 70
+check = age >= 18 and age < 45 # одно из условий не верно и в результате check - false
+print("check =", check)
