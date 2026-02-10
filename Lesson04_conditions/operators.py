c17f44bdef404ce3b72a12087d753c47ebb96228
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-10 10:38:07 +0100

    # or

--- Lesson04_conditions/operators.py
+++ Lesson04_conditions/operators.py
@@ -14,14 +14,39 @@ print("a =", a)
 
 # !! logical operators "and", "or", "not"
 
+# and
+
 age = 27
-check = age >= 18 and age < 45  # оба условия верны и в результате check - true
+check = age >= 18 and age < 45  # оба условия верны и в результате 'check' - 'true'
 print("check =", check)
 
 age = 16
-check = age >= 18 and age < 45 # одно из условий не верно и в результате check - false
+# одно из условий не верно и в результате 'check' - 'false'
+check = age >= 18 and age < 45
 print("check =", check)
 
 age = 70
-check = age >= 18 and age < 45 # одно из условий не верно и в результате check - false
+# одно из условий не верно и в результате 'check' - 'false'
+check = age >= 18 and age < 45
 print("check =", check)
+
+# or
+
+age = 21
+check = age < 18 or age >= 45
+print(f"check if {age} = {check}")
+
+age = 18
+check = age < 18 or age >= 45
+print(f"check if {age} = {check}")
+
+age = 70
+check = age < 18 or age >= 45
+print(f"check if {age} = {check}")
+
+time = True
+money = True
+sponsor = False
+traveling = True
+traveling = time and (money or sponsor)
+print(f"traveling is posible - {traveling}")
