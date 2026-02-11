2f2bab38e5e5f3859c12e7eada1a2e06f053ef38
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-11 11:23:36 +0100

    def check_user_number task 04

--- Lesson05_practice/task_03.py
+++ Lesson05_practice/task_03.py
@@ -1,25 +1,12 @@
-# Пользователь вводит с консоли число (input), програма проверяет число (if-else)
-# и печатаете в консоль (print) сообщение число четное или нечетное
-# четное число если делится без остатка на 2, нечетное в протичном случаи (используйте %)
 
-# entered_number = int(input("Enter you number: "))
-# if entered_number % 2 == 0:
-#     print(f"Number {entered_number} is even number")
-# else:
-#     print(f"Number {entered_number} is odd number")
+# Пользователь вводит с консоли число (input) - свою часовую ставку оплаты (wage)
+# Потом вводит в консоль сколько часов он отработал за месяц
+# Программа рассчитывает и выводит в консоль (print) зарплату пользователя
 
+wage = float(input("Enter your hourly rate of pay: "))
+hours_worked = float(
+    input("Enter the number of hours you worked: "))
 
-def check_input_number():
-    try:
-        entered_number = input("Enter you number: ")
-        number = int(entered_number)
-        if number % 2 == 0:
-            print(f"Number {number} is even number")
-        else:
-            print(f"Number {number} is odd number")
+salary = wage * hours_worked
 
-    except ValueError:
-        print("\033[31mError: You must enter an integer!\033[0m")
-
-
-# check_input_number()
+print(f"Your salary for this month was - {salary}$")
---
+++ print_colors.py
@@ -0,0 +1,48 @@
+# print_colors.py
+
+# ANSI коды для цветов
+from print_colors import red, green, blue, bold
+RESET = "\033[0m"
+BOLD = "\033[1m"
+
+# Обычные цвета
+
+
+def black(text): return f"\033[30m{text}{RESET}"
+def red(text): return f"\033[31m{text}{RESET}"
+def green(text): return f"\033[32m{text}{RESET}"
+def yellow(text): return f"\033[33m{text}{RESET}"
+def blue(text): return f"\033[34m{text}{RESET}"
+def magenta(text): return f"\033[35m{text}{RESET}"
+def cyan(text): return f"\033[36m{text}{RESET}"
+def white(text): return f"\033[37m{text}{RESET}"
+
+# Яркие цвета
+
+
+def bright_black(text): return f"\033[90m{text}{RESET}"
+def bright_red(text): return f"\033[91m{text}{RESET}"
+def bright_green(text): return f"\033[92m{text}{RESET}"
+def bright_yellow(text): return f"\033[93m{text}{RESET}"
+def bright_blue(text): return f"\033[94m{text}{RESET}"
+def bright_magenta(text): return f"\033[95m{text}{RESET}"
+def bright_cyan(text): return f"\033[96m{text}{RESET}"
+def bright_white(text): return f"\033[97m{text}{RESET}"
+
+# Дополнительно: жирный текст
+
+
+def bold(text): return f"{BOLD}{text}{RESET}"
+
+
+# Пример использования при запуске файла напрямую
+if __name__ == "__main__":
+    print(red("Ошибка!"))
+    print(green("Успех!"))
+    print(blue("Информация"))
+    print(bold("Жирный текст"))
+
+
+earnings = 1500
+print(f"Your salary: {green(earnings)}$ - {red('Not paid yet')}")
+print(bold("This is important"))
