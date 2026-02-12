66b272ad878330162b61eb94923558bbc1ae1e38
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-12 13:35:25 +0100

    task 05 fix

--- Lesson06_practice/task_05.py
+++ Lesson06_practice/task_05.py
@@ -5,8 +5,9 @@ def bank_work_time(day_of_week):
         return "8:30 - 13:30"
     if day_of_week == 2 or day_of_week == 4:
         return "8:30 - 13:30 и 16:00 - 18:00"
-    else:
-        return "Неверный номер дня недели!"
+    # else:
+    #     return "Неверный номер дня недели!"
+    return "Неверный номер дня недели!"
     
 day = int(input("Введите номер дня недели: "))
 result = bank_work_time(day)
