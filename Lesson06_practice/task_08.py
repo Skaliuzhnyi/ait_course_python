beb58d5bc3e24e9f2691b6f2fd2e52cc0faf33d5
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-13 13:54:02 +0100

    def check_team (new code)

--- Lesson06_practice/task_08.py
+++ Lesson06_practice/task_08.py
@@ -10,27 +10,45 @@
 # 3. Вызвать функцию с введенными данными и результат сохранить в переменную
 # 4. Распечатать значение переменной в консоли
 
+# def check_team(week):
+#     team_John = 'John'
+#     team_Peter = 'Peter'
+#     team_Mary = 'Mary'
+#     team_Ann = 'Ann'
+
+#     weeks1 = list(range(1, 53, 4))
+#     weeks2 = list(range(2, 53, 4))
+#     weeks3 = list(range(3, 53, 4))
+#     weeks4 = list(range(4, 53, 4))
+
+#     if week in weeks1:
+#         return team_John
+#     elif week in weeks2:
+#         return team_Peter
+#     elif week in weeks3:
+#         return team_Mary
+#     elif week in weeks4:
+#         return team_Ann
+#     else:
+#         return "Error"
+
+
+# week_number = int(input("Введите номер недели: "))
+# result = check_team(week_number)
+# print(
+#     f"Имя тимлида команда которого работает на соответствующей неделе - {result}" if result != "Error" else " Вы ввели не корректное значение недели!")
+
 def check_team(week):
-    team_John = 'John'
-    team_Peter = 'Peter'
-    team_Mary = 'Mary'
-    team_Ann = 'Ann'
 
-    weeks1 = list(range(1, 53, 4))
-    weeks2 = list(range(2, 53, 4))
-    weeks3 = list(range(3, 53, 4))
-    weeks4 = list(range(4, 53, 4))
-
-    if week in weeks1:
-        return team_John
-    elif week in weeks2:
-        return team_Peter
-    elif week in weeks3:
-        return team_Mary
-    elif week in weeks4:
-        return team_Ann
-    else:
+    if week > 52 or week < 1:
         return "Error"
+    if week % 4 == 1:
+        return "John"
+    if week % 4 == 2:
+        return "Peter"
+    if week % 4 == 3:
+        return "Mary"
+    return "Ann"
 
 
 week_number = int(input("Введите номер недели: "))
