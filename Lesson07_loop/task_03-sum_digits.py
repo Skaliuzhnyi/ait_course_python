37097f81c9aeb631344a13d098c40f2cfa0f0647
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-16 15:01:33 +0100

    fix

--- Lesson07_loop/task_03-sum_digits.py
+++ Lesson07_loop/task_03-sum_digits.py
@@ -1,5 +1,17 @@
 # sum_digits(5712348)
 
+# def sum_digits(n):
+#     total = 0
+#     for digit in str(n):  # Преобразуем число в строку и проходим по каждому символу
+#         # Преобразуем символ обратно в число и добавляем к общей сумме
+#         total += int(digit)
+#     return total
+
+
+# number = int(input("\033[31mВведите число: \033[0m"))
+# sum_result = sum_digits(number)
+# print("\033[33mСумма цифр: \033[0m", sum_result)
+
 def sum_digits(n):
     total = 0
     while n > 0:
