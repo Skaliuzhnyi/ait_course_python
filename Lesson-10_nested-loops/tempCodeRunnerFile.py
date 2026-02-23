d641131fb64e2cde9634f039614f524a67d2b536
Serhii Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2026-02-23 10:51:18 +0100

    Lesson 11 add / bubble_sort

--- Lesson-10_nested-loops/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-print(_)
-      # if _ == 0 or _ == n - 1:
-      #     print('* ' * n)
-      # elif _ > 0 and _ < n - 1:
-      #     print('* ' + '  ' * (n - 2) + '* ')
---
+++ Lesson-11_sorting/task_01-sorting.py
@@ -0,0 +1,19 @@
+
+import random
+
+start = 10
+end = 99
+quantity = 10
+random_list = [random.randint(start, end) for _ in range(quantity)]
+print(random_list)
+
+
+def bubble_sort(lst):
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+
+bubble_sort(random_list)
+print(random_list)
